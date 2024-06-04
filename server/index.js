b3b4d5d3022fe7273a0b14f52eeea9d45aaf6906
Stovencrantz <sknapp143@gmail.com>
2024-06-04 19:15:31 -0400

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const router = require('./routes');
+app.use("/api", router);
+
+app.get("/", (req, res) => {
+  res.status(200).json({msg: "hello peoples"});
+});
+
+const port = 5000;
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
