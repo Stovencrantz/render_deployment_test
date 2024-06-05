a1a5871fd1f0e356c11ec2b8bffbf36e9e78c789
Steven Knapp <sknapp@Stevens-MacBook-Pro.local>
2024-06-04 21:11:19 -0400

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,18 @@
+import {useEffect} from "react";
 
 function App() {
+
+  useEffect(() => {
+    async function getTodos() {
+      const res = await fetch("http://localhost:2000/api/todos");
+      const todos = await res.json();
+
+      console.log(todos)
+    }
+    getTodos()
+  },[]) 
+
+
   return (
     <main className="container">
       <h1>Awsesome Todos</h1>
--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   res.status(200).json({msg: "hello peoples"});
 });
 
-const port = 5000;
+const port = 2000;
 app.listen(port, () => {
   console.log(`Server is listening on http://localhost:${port}`);
 });
